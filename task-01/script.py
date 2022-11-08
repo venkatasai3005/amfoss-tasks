0609ed915546de1a93ac77e35ab00603554fdd3b
venkatasai3005 <117254931+venkatasai3005@users.noreply.github.com>
2022-11-08 14:40:55 +0530

    Create script.py

---
+++ task-01/script.py
@@ -0,0 +1,48 @@
+# Script we will get is
+
+script.py                                            100%[=====================================================================================================================>]   1.34K  --.-KB/s    in 0s      
+
+2022-11-08 12:30:25 (39.7 MB/s) - ‘script.py’ saved [1369/1369]
+
+
+
+Starring amfoss/kdeconf
+Starring amfoss/website_old
+Starring amfoss/cybergurukulam
+Starring amfoss/Wikimedia_Hackathon_Amrita_University
+Starring amfoss/fosswebsite
+Starring amfoss/fosster
+Starring amfoss/App
+Starring amfoss/automated-scripts
+Starring amfoss/master-syllabus
+Starring amfoss/Workshops
+Starring amfoss/foss-meetups
+Starring amfoss/vidyaratna
+Starring amfoss/android-workshop-summer-2018
+Starring amfoss/test-repo
+Starring amfoss/Foss-talks
+Starring amfoss/amdec-website
+Starring amfoss/GitLit
+Starring amfoss/cms
+Starring amfoss/WebApp
+Starring amfoss/Praveshan
+Starring amfoss/star-me
+Starring amfoss/bot
+Starring amfoss/Hack4Amrita
+Starring amfoss/website
+Starring amfoss/Qujini
+Starring amfoss/TempleApp
+Starring amfoss/internal-hackathon
+Starring amfoss/events
+Starring amfoss/attendance-tracker
+Starring amfoss/cms-mobile
+
+
+    Thank you None (@venkatasai3005) for starring all the amFOSS repositories.
+     ____  _                  _               ____                   
+    / ___|| |_ __ _ _ __ _ __(_)_ __   __ _  |  _ \  ___  _ __   ___ 
+    \___ \| __/ _` | '__| '__| | '_ \ / _` | | | | |/ _ \| '_ \ / _ \
+     ___) | || (_| | |  | |  | | | | | (_| | | |_| | (_) | | | |  __/
+    |____/ \__\__,_|_|  |_|  |_|_| |_|\__, | |____/ \___/|_| |_|\___|
+                                       |___/                          
+
